9111a690032fa46d7b65732eb7bc3ab6520fd9ae
Michael Marod <mmarod@devis.com>
2015-05-13 18:33:25 +0000

    add cron entry

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,31 +1,56 @@
 # This class configures logcheck
 #
-# @example Standard configuration
-#   include logcheck
-#   logcheck::package: logcheck
-#   logcheck::configfile: /etc/logcheck/logcheck.conf
+# @example Standard configuration with Hiera
+#   include '::logcheck'
+#
 #   logcheck::config:
 #     REPORTLEVEL: server
 #     SENDMAILTO: logcheck@example.com
 #     ADDTAG: 'yes'
 #
-# @param package [Array] The name of the logcheck package
+# @example Standard configuration directly setting variables
+#   class { 'logcheck':
+#     config => {
+#       'REPORTLEVEL'   => 'server',
+#       'SENDMAILTO'    => 'logcheck@example.com',
+#       'ADDTAG'        => 'yes'
+#     }
+#   }
+#
+# @param package [String] The name of the logcheck package
+# @param user [String] The name of the logcheck user
 # @param configfile [String] The path to logcheck.conf
 # @param config [Hash] A hash of changes to make to logcheck.conf
+# @param crons [Hash] A hash of crons to run logcheck
 #
 class logcheck (
     $package        = 'logcheck',
+    $user           = 'logcheck',
     $configfile     = '/etc/logcheck/logcheck.conf',
     $config         = {},
+    $crons          = {
+                        'logcheck' => {
+                            'command'   => 'if [ -x /usr/sbin/logcheck ]; then nice -n10 /usr/sbin/logcheck -R; fi',
+                            'user'      => 'logcheck',
+                            'minute'    => 2
+                        }, 'logcheck-reboot' => {
+                            'command'   => 'if [ -x /usr/sbin/logcheck ]; then nice -n10 /usr/sbin/logcheck -R; fi',
+                            'user'      => 'logcheck',
+                            'special'   => 'reboot'
+                        }
+                      }
 ){
     validate_string($::logcheck::packages)
+    validate_string($::logcheck::user)
     validate_absolute_path($::logcheck::configfile)
     validate_hash($::logcheck::config)
+    validate_hash($::logcheck::crons)
 
     $changes_array = join_keys_to_values($::logcheck::config, ' ')
     $changes = regsubst($changes_array,'.*','set \0')
 
     ensure_packages($::logcheck::package)
+    ensure_resource('user', $::logcheck::user, {'ensure' => 'present'})
 
     augeas { 'configure-logcheck':
         incl    => $::logcheck::configfile,
@@ -33,4 +58,6 @@ class logcheck (
         changes => $::logcheck::changes,
         require => File[$::logcheck::configfile]
     }
+
+    create_resources('cron', $::logcheck::crons)
 }
--- spec/classes/logcheck_spec.rb
+++ spec/classes/logcheck_spec.rb
@@ -16,6 +16,22 @@ describe 'logcheck' do
     should contain_package('logcheck')
   end
 
+  it do 
+    should contain_user('logcheck')
+  end
+
+  it do 
+    should contain_cron('logcheck') \
+      .with_command('if [ -x /usr/sbin/logcheck ]; then nice -n10 /usr/sbin/logcheck -R; fi') \
+      .with_user('logcheck') \
+      .with_minute(2)
+
+    should contain_cron('logcheck-reboot') \
+      .with_command('if [ -x /usr/sbin/logcheck ]; then nice -n10 /usr/sbin/logcheck -R; fi') \
+      .with_user('logcheck') \
+      .with_special('reboot')
+  end
+
   it 'should have an augeas resource' do
     should contain_augeas('configure-logcheck')
   end
